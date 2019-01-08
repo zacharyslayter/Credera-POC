eb4f99f298ea9352af4f1d084009583b1dab74f4
Melanie Cummings <mec948@gmail.com>
2019-01-08 09:57:04 -0600

    adding border to button buttons functionality

--- Credera-POC/Storyboard/ErrorScreen/Controller/ErrorScreenViewController.swift
+++ Credera-POC/Storyboard/ErrorScreen/Controller/ErrorScreenViewController.swift
@@ -11,13 +11,32 @@ import UIKit
 
 class ErrorScreenViewController: UIViewController {
     
+    @IBOutlet var backToHomeButton: UIButton!
+    @IBOutlet var backButton: UIButton!
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
+        
+        backButton.layer.cornerRadius = 5
+        backToHomeButton.layer.cornerRadius = 5
+        
+        backButton.layer.borderWidth = 2
+        backToHomeButton.layer.borderWidth = 2
+        
+        backToHomeButton.layer.borderColor = Constants.ColorScheme.lipstick.cgColor
+        backButton.layer.borderColor = Constants.ColorScheme.lipstick.cgColor
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    @IBAction func backToHomeSelected(_ sender: Any) {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+    
+    @IBAction func backButtonSelected(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
 }
 
