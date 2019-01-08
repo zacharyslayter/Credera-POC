92d5425c07c6ac8ce274e3f6cef6cfab9529daa7
Natasha Solanki <natasha.solanki@credera.com>
2019-01-08 10:25:12 -0600

    guest name input in progress

--- Credera-POC/Storyboard/WelcomeScreen/Controller/WelcomeScreenViewController.swift
+++ Credera-POC/Storyboard/WelcomeScreen/Controller/WelcomeScreenViewController.swift
@@ -51,22 +51,23 @@ class WelcomeScreenViewController: UIViewController {
     }
     
     @IBAction func meetingSelected(_ sender: Any) {
-        //Go to the who are you meeting with screen
-        let storyboard = UIStoryboard(name: "EmployeeSearch", bundle: nil)
-        let employeeSearchVC = storyboard.instantiateViewController(withIdentifier: "EmployeeSearchViewController") as! EmployeeSearchViewController
-        navigationController?.pushViewController(employeeSearchVC, animated: true)
+        let storyBoard : UIStoryboard = UIStoryboard(name: "First", bundle:nil)
+        let interviewVC = storyBoard.instantiateViewController(withIdentifier: "FirstViewController") as! FirstViewController
+        self.navigationController?.pushViewController(interviewVC, animated: true)
     }
     
     @IBAction func specificPersonSelected(_ sender: Any) {
-        let storyBoard : UIStoryboard = UIStoryboard(name: "First", bundle:nil)
-        let personVC = storyBoard.instantiateViewController(withIdentifier: "FirstViewController") as! FirstViewController
-        self.navigationController?.pushViewController(personVC, animated: true)
+        let storyboard = UIStoryboard(name: "EmployeeSearch", bundle: nil)
+        let employeeSearchVC = storyboard.instantiateViewController(withIdentifier: "EmployeeSearchViewController") as! EmployeeSearchViewController
+        employeeSearchVC.searchType = .specificPerson
+        navigationController?.pushViewController(employeeSearchVC, animated: true)
     }
     
     @IBAction func deliverySelected(_ sender: Any) {
-        let storyBoard : UIStoryboard = UIStoryboard(name: "First", bundle:nil)
-        let deliveryVC = storyBoard.instantiateViewController(withIdentifier: "FirstViewController") as! FirstViewController
-        self.navigationController?.pushViewController(deliveryVC, animated: true)
+        let storyboard = UIStoryboard(name: "EmployeeSearch", bundle: nil)
+        let employeeSearchVC = storyboard.instantiateViewController(withIdentifier: "EmployeeSearchViewController") as! EmployeeSearchViewController
+        employeeSearchVC.searchType = .delivery
+        navigationController?.pushViewController(employeeSearchVC, animated: true)
     }
     
     @IBAction func otherSelected(_ sender: Any) {
