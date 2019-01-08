b56ca1e56e0a3af5c8b59e20b2b1dd5c5668f872
Melanie Cummings <mec948@gmail.com>
2019-01-08 08:46:26 -0600

    Added error view and storyboard - no constraints yet

---
+++ Credera-POC/Storyboard/ErrorScreen/Controller/ErrorScreenViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ErrorScreenViewController.swift
+//  Credera-POC
+//
+//  Created by Melanie Cummings on 1/8/19.
+//  Copyright © 2019 Credera. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ErrorScreenViewController: UIViewController {
+    
+}
+
